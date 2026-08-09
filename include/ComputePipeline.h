aaad259dae1bd0467d474d787dffc78617196260
Jeff613 <143241899+Jeffwngl@users.noreply.github.com>
2026-08-09 16:29:20 +1000

    added compute pipeline initialization

---
+++ include/ComputePipeline.h
@@ -0,0 +1,21 @@
+#include "Core.h"
+
+#include <string>
+#include <vulkan/vulkan_core.h>
+
+class ComputePipeline {
+public:
+    ComputePipeline = default;
+    ~ComputePipeline;
+
+    void initialize(VulkanCore& vkCore, std::string path, VkDescriptorSetLayout descriptorSetLayout);
+
+    VkPipeline getPipeline();
+    VkPipelineLayout getPipelineLayout();
+
+private:
+    VulkanCore* vulkanCore;
+    VkPipeline pipeline{ VK_NULL_HANDLE };
+    VkPipelineLayout pipelineLayout{ VK_NULL_HANDLE };
+}
+
---
+++ src/ComputePipeline.cpp
@@ -0,0 +1,85 @@
+#include "ComputePipeline.h"
+#include "Utils.h"
+#include <vulkan/vulkan_core.h>
+
+ComputePipeline::~ComputePipeline() {
+    if (!vulkanCore) {
+        return;
+    };
+
+    VkDevice device = vulkanCore->getDevice();
+
+    if (pipeline != VK_NULL_HANDLE) {
+        vkDestroyPipeline(device, pipeline, nullptr);
+    }
+
+    if (pipelineLayout != VK_NULL_HANDLE) {
+        vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
+    }
+}
+
+void ComputePipeline::initialize(VulkanCore& vkCore, std::string path, VkDescriptorSetLayout descriptorSetLayout) {
+    vulkanCore = &vkCore;
+
+    auto computeShader = utils::readFile(path); // readfile
+
+    VkShaderModuleCreateInfo shaderModuleCI{
+        .sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO,
+        .codeSize = computeShader.size(),
+        .pCode = // computeShader data
+    };
+
+    VkShaderModule shaderModule;
+
+    utils::check(vkCreateShaderModule(
+        vulkanCore->getDevice(),
+        &shaderModuleCI,
+        VK_NULL_HANDLE,
+        &shaderModule
+    ));
+
+    VkPipelineLayoutCreateInfo pipelineLayoutCI{
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO,
+        .setLayoutCount = 1,
+        .pSetLayouts = &descriptorSetLayout
+    };
+
+    utils::check(vkCreatePipelineLayout(
+        vulkanCore->getDevice(),
+        &pipelineLayoutCI,
+        VK_NULL_HANDLE,
+        &pipelineLayout
+    ));
+
+    VkPipelineShaderStageCreateInfo shaderStageCI{
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+        .stage = VK_SHADER_STAGE_COMPUTE_BIT,
+        .module = shaderModule,
+        .pName = "main"
+    };
+
+    VkComputePipelineCreateInfo computePipelineCI{
+        .sType = VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO,
+        .layout = pipelineLayout,
+        .stage = shaderStageCI
+    };
+
+    utils::check(vkCreateComputePipelines(
+        vulkanCore->getDevice(),
+        VK_NULL_HANDLE,
+        1,
+        &computePipelineCI,
+        VK_NULL_HANDLE,
+        &pipeline
+    ));
+
+    vkDestroyShaderModule(vulkanCore->getDevice(), shaderModule, VK_NULL_HANDLE);
+}
+
+VkPipeline ComputePipeline::getPipeline() {
+    return pipeline;
+}
+
+VkPipelineLayout ComputePipeline::getPipelineLayout() {
+    return pipelineLayout;
+}

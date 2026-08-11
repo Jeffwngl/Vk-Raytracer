e257d8c39eb29dd72ec55bda15293e4159731d79
Jeff613 <143241899+Jeffwngl@users.noreply.github.com>
2026-08-11 21:43:35 +1000

    added unfinished renderer file to continue working on renderer in other computer

---
+++ include/Renderer.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "Core.h"
+#include "ComputePipeline.h"
+#include "ComputeDescriptorSet.h"
+
+#include <iostream>
+#include <stdbool.h>
+#include <stdint.h>
+
+class Renderer {
+public:
+    Renderer() = default;
+    ~Renderer();
+
+    bool initialize(VulkanCore& vkCore);
+
+    void drawFrame(FrameData& frame);
+    void cleanUp();
+
+private:
+    void waitForFences(FrameData& frame);
+    bool acquireSwapchainImage(FrameData& frame, uint32_t& imageIndex);
+    void resetFences(FrameData& frame);
+    void resetCommandBuffers(FrameData& frame);
+    void recordCommandBuffers(VkCommandBuffer commandBuffer, uint32_t imageIndex);
+    void submitCommandBuffers();
+    void presentSwapchainImage();
+
+    void createOutputImage();
+    void createOutputImageView();
+    // https://docs.vulkan.org/guide/latest/storage_image_and_texel_buffers.html
+    // transition outputImage from undefined to general
+    void transitionImage(
+		VkCommandBuffer commandBuffer,
+		VKImage image,
+		VkImageLayout oldLayout,
+		VkImageLayout newLayout
+	);
+
+private:
+    VulkanCore* vulkanCore{ nullptr };
+    uint32_t currentFrame{ 0 };
+
+    ComputePipeline computePipeline;
+    ComputeDescriptorSet computeDescriptorSet;
+    VkImage outputImage{ VK_NULL_HANDLE };
+    VkImageView outputImageView{ VK_NULL_HANDLE };
+    VmaAllocation outputImageAllocation{ VK_NULL_HANDLE };
+};
---
+++ src/Renderer.cpp
@@ -0,0 +1,296 @@
+#include "Renderer.h"
+
+#include <cstdint>
+#include <string>
+#include <vulkan/vulkan_core.h>
+
+#include "Utils.h"
+
+bool Renderer::initialize(VulkanCore& vkCore) {
+    vulkanCore = &vkCore;
+    
+    // use separate image view from swapchain to avoid platform specifics
+    createOutputImage();
+    createOutputImageView();
+    
+    std::string path = "path/to/shader";
+    
+    computeDescriptorSet.initialize(*vulkanCore, outputImageView);
+    computePipeline.initialize(*vulkanCore, path, computeDescriptorSet.getDescriptorSetLayout());
+
+    return true;
+}
+
+void Renderer::drawFrame(FrameData& frame) {
+    waitForFences(frame);
+
+    uint32_t imageIndex;
+    
+    if (!acquireSwapchainImage(frame, imageIndex)) {
+        return;
+    }
+
+    resetFences(frame);
+    resetCommandBuffers(frame);
+    
+
+}
+
+
+void Renderer::createOutputImage() {
+    VkImageCreateInfo imageCI{
+        .sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO,
+        .imageType = VK_IMAGE_TYPE_2D,
+        .format = VK_FORMAT_R8G8B8A8_UNORM,
+
+        .extent = {
+            .width = static_cast<uint32_t>(vulkanCore->getWindowSize().x),
+            .height = static_cast<uint32_t>(vulkanCore->getWindowSize().y),
+            .depth = 1
+        },
+
+        .mipLevels = 1,
+        .arrayLayers = 1,
+
+        .samples = VK_SAMPLE_COUNT_1_BIT,
+        .tiling = VK_IMAGE_TILING_OPTIMAL,
+
+        .usage =
+            VK_IMAGE_USAGE_STORAGE_BIT |
+            VK_IMAGE_USAGE_TRANSFER_SRC_BIT,
+
+        .sharingMode = VK_SHARING_MODE_EXCLUSIVE,
+
+        .initialLayout = VK_IMAGE_LAYOUT_UNDEFINED
+    };
+
+
+    vkCreateImage(
+        vulkanCore->getDevice(),
+        &imageCI,
+        nullptr,
+        &outputImage
+    );
+}
+
+void Renderer::createOutputImageView() {
+    VkImageViewCreateInfo imageViewCI{
+        .sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
+        .image = outputImage,
+        .viewType = VK_IMAGE_VIEW_TYPE_2D,
+        .format = VK_FORMAT_R8G8B8A8_UNORM,
+        .subresourceRange = {
+            .aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+            .baseMipLevel = 0,
+            .levelCount = 1,
+            .baseArrayLayer = 0,
+            .layerCount = 1
+        }
+    };
+
+    utils::check(vkCreateImageView(
+        vulkanCore->getDevice(),
+        &imageViewCI,
+        nullptr,
+        &outputImageView
+    ));
+}
+
+void Renderer::waitForFences(FrameData& frame) {
+    vkWaitForFences(
+        vulkanCore->getDevice(),
+        1,
+        &frame.computeFence,
+        VK_TRUE,
+        UINT64_MAX
+    );
+
+    vkWaitForFences(
+        vulkanCore->getDevice(),
+        1,
+        &frame.graphicsFence,
+        VK_TRUE,
+        UINT64_MAX
+    );
+}
+
+bool Renderer::acquireSwapchainImage(FrameData& frame, uint32_t& imageIndex) {
+    VkResult result = vkAcquireNextImageKHR(
+        vulkanCore->getDevice(),
+        vulkanCore->getSwapchain(),
+        UINT64_MAX,
+        frame.imageAvailable,
+        VK_NULL_HANDLE,
+        &imageIndex
+    );
+
+    if (result == VK_ERROR_OUT_OF_DATE_KHR) {
+        // TODO: handle out of date case
+        return false;
+    }
+    else if (result != VK_SUCCESS && result != VK_SUBOPTIMAL_KHR) {
+        throw std::runtime_error("failed to acquire swap chain image!");
+    }
+
+    return true;
+}
+
+void Renderer::resetFences(FrameData& frame) {
+    vkResetFences(
+        vulkanCore->getDevice(),
+        1,
+        &frame.computeFence
+    );
+
+    vkResetFences(
+        vulkanCore->getDevice(),
+        1,
+        &frame.graphicsFence
+    );
+}
+
+void Renderer::resetCommandBuffers(FrameData& frame) {
+    vkResetCommandBuffer(frame.computeCommandBuffer, 0);
+    vkResetCommandBuffer(frame.graphicsCommandBuffer, 0);
+}
+
+void Renderer::recordCommandBuffers(VkCommandBuffer commandBuffer, uint32_t imageIndex) {
+    VkCommandBufferBeginInfo beginInfo{
+        .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO
+    };
+
+    utils::check(vkBeginCommandBuffer(commandBuffer, &beginInfo));
+
+	VkImage swapchainImage = vulkanCore->getSwapchainImages()[imageIndex];
+
+    uint32_t width = static_cast<uint32_t>(vulkanCore->getWindowSize().x);
+    uint32_t height = static_cast<uint32_t>(vulkanCore->getWindowSize().y);
+
+	transitionImage(
+		commandBuffer,
+		outputImage,
+		VK_IMAGE_LAYOUT_UNDEFINED,
+		VK_IMAGE_LAYOUT_GENERAL
+	);
+
+    vkCmdBindPipeline(
+        commandBuffer,
+        VK_PIPELINE_BIND_POINT_COMPUTE,
+        computePipeline.getPipeline()
+    );
+
+
+    VkDescriptorSet descriptorSet = computeDescriptorSet.getDescriptorSet();
+
+    vkCmdBindDescriptorSets(
+        commandBuffer,
+        VK_PIPELINE_BIND_POINT_COMPUTE,
+        computePipeline.getPipelineLayout(),
+        0,
+        1,
+        &descriptorSet,
+        0,
+        nullptr
+    );
+
+	vkCmdDispatch(
+		commandBuffer,
+		width / 16,
+		height / 16,
+		1
+	);
+	
+	transitionImage( // prepare output image
+		commandBuffer,
+		outputImage,
+		VK_IMAGE_LAYOUT_GENERAL,
+		VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL
+	);
+
+	transitionImage( // prepare swapchain image for same format
+		commandBuffer,
+		swapchainImage,
+		VK_IMAGE_LAYOUT_UNDEFINED,
+		VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL
+	);
+
+	VkImageCopy region{
+        .srcSubresource = {
+            .aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+            .mipLevel = 0,
+            .baseArrayLayer = 0,
+            .layerCount = 1
+        },
+        .dstSubresource = {
+            .aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+            .mipLevel = 0,
+            .baseArrayLayer = 0,
+            .layerCount = 1
+        },
+        .extent = {
+            .width = width,
+            .height = height,
+            .depth = 1
+        }
+	};
+
+    vkCmdCopyImage(
+        commandBuffer,
+        outputImage,
+        VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
+        swapchainImage,
+        VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+        1,
+        &region
+    );
+
+    // prepare for vkQueuePresentKHR
+    transitionImage(
+        commandBuffer,
+        swapchainImage,
+        VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+        VK_IMAGE_LAYOUT_PRESENT_SRC_KHR
+    );
+
+    utils::check(vkEndCommandBuffer(commandBuffer));
+}
+
+void Renderer::transitionImage(
+		VkCommandBuffer commandBuffer,
+		VkImage image,
+		VkImageLayout oldLayout,
+		VkImageLayout newLayout
+) {
+	VkImageMemoryBarrier2 barrier{
+		.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER_2,
+		.srcStageMask = VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT,
+		.srcAccessMask = VK_ACCESS_2_MEMORY_WRITE_BIT,
+		.dstStageMask = VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT,
+		.dstAccessMask = VK_ACCESS_2_MEMORY_WRITE_BIT | VK_ACCESS_2_MEMORY_READ_BIT,
+		.oldLayout = oldLayout,
+		.newLayout = newLayout,
+		.image = image,
+		.subresourceRange = {
+			.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+			.baseMipLevel = 0,
+			.levelCount = 1,
+			.baseArrayLayer = 0,
+			.layerCount = 1
+		}
+	};
+	
+    VkDependencyInfo dependencyInfo{
+        .sType = VK_STRUCTURE_TYPE_DEPENDENCY_INFO,
+        .imageMemoryBarrierCount = 1,
+        .pImageMemoryBarriers = &barrier
+    };
+
+    vkCmdPipelineBarrier2(
+        commandBuffer,
+        &dependencyInfo
+    );
+}
+
+void Renderer::cleanUp() {
+	
+}
